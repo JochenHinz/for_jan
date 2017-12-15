8717648fb8e161cdcf08f387599c2222a6cf847a
JochenHinz <hickslebummbumm@gmx.net>
2017-12-15 16:03:24 +0100

    stable version

--- grid_tester.py
+++ grid_tester.py
@@ -1,1 +1,84 @@
-../Grid_generator/grid_tester.py
+from nutils import *
+import utilities as ut
+import numpy as np
+import scipy as sp
+import problem_library as pl
+import Solver
+import preprocessor as prep
+from auxilliary_classes import *
+numpy.set_printoptions(threshold=numpy.nan)
+
+
+def grid_convergence(go, load = None):
+    domain, geom, basis, s, p, ischeme = go.domain, go.geom, go.basis, go.s, go.degree, go.ischeme
+    if not load:
+        load = numpy.ones(len(basis))
+    mapping = basis.vector(2).dot(s)
+    funcs = []
+    for i in range(3):
+        if i != 0:
+            domain = domain.refine(1)
+            basis = domain.basis('spline', degree = p)
+        mat = domain.integrate(function.outer( basis.grad(mapping,ndims=2), basis.grad(mapping,ndims=2) ).sum(2), geometry = mapping, ischeme = gauss(ischeme))
+        rhs = domain.integrate(basis,geometry = geom, ischeme = gauss(ischeme))
+        cons = domain.boundary.project(0, onto = basis, geometry = geom, ischeme = gauss(ischeme))
+        lhs = mat.solve(rhs, constrain=cons, symmetric = True)
+        funcs.append(basis.dot(lhs))
+    diff1, diff2 = np.sqrt(domain.integrate([(funcs[1] - funcs[0])**2, (funcs[2] - funcs[1])**2], geometry = geom, ischeme = gauss(ischeme)))
+    return np.log(diff1/diff2)/np.log(2)
+
+
+def flatten_weights_to_gismo(go):
+    assert len(go) in [1,2], NotImplementedError
+    s, knots, p = go.s, go.knots, go.degree
+    l = len(s) // go.repeat
+    ss = [vec.reshape(go.ndims).T.flatten()[:,None] for vec in chunks(s,l)]
+    return str(np.hstack(ss)).replace('[','').replace(']','')
+
+
+def export_to_gismo(go):
+    if len(go) == 2:
+        return export_to_gismo_bivariate(go)
+    elif len(go) == 1:
+        return export_to_gismo_univariate(go)
+    raise NotImplementedError
+
+
+def export_to_gismo_bivariate(go):
+    p, knots = go.degree, go._knots
+    knots_xi, knots_eta = [(str(kn.extend_knots())[1:-1]).replace(',','') for kn in knots]
+    string = '<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n' \
+             ' <xml> \n' \
+             '   <Geometry type=\"TensorBSpline2\" id=\"0\"> \n' \
+             '   <Basis type=\"TensorBSplineBasis2\"> \n' \
+            '     <Basis type=\"BSplineBasis\" index=\"0\"> \n' \
+            '<KnotVector degree=\"' + str(p[0]) + '\"> ' + knots_xi +' </KnotVector> \n' \
+            '     </Basis> \n' \
+            '     <Basis type=\"BSplineBasis\" index=\"1\"> \n' \
+            '<KnotVector degree=\"' + str(p[1]) + '\"> ' + knots_eta +' </KnotVector> \n' \
+            '     </Basis> \n' \
+            '    </Basis> \n' \
+            '    <coefs geoDim=\"' + str(go.repeat) + '\"> \n'\
+            +flatten_weights_to_gismo(go)+'\n' \
+            '    </coefs> \n' \
+            '   </Geometry> \n' \
+            '</xml>'
+    return string
+
+
+def export_to_gismo_univariate(go):
+    assert len(go) == 1
+    p, knots = go.degree[0], go._knots
+    knots_xi = (str(knots.extend_knots()[0])[1:-1]).replace(',','')
+    string = '<?xml version="1.0" encoding="UTF-8"?> \n' \
+             '    <xml> \n' \
+             '    <Geometry type="BSpline"> \n' \
+             '    <Basis type="BSplineBasis"> \n' \
+            '    <KnotVector degree=\"' + str(p) + '\"> ' + knots_xi +' </KnotVector> \n' \
+             '    </Basis> \n' \
+            '   <coefs geoDim=\"' + str(go.repeat) + '\"> \n' \
+            + flatten_weights_to_gismo(go) + '\n' \
+            '</coefs> \n' \
+            '</Geometry> \n' \
+            '</xml>'
+    return string
--- test_bench.py
+++ test_bench.py
@@ -1,1 +1,57 @@
-../Grid_generator/test_bench.py
+import numpy as np
+import scipy as sp
+from nutils import *
+from auxilliary_classes import *
+import utilities as ut
+
+
+def square_go(ndims,p):
+    kv = np.prod([ut.nonuniform_kv(p, knotvalues = np.linspace(0,1,n+1)) for n in ndims])
+    domain, geom = mesh.rectilinear(kv.knots)
+    basis = domain.basis('bspline', degree = p, knotvalues = kv.knots).vector(2)
+    cons = domain.boundary.project(geom, geometry = geom, onto = basis, ischeme = 'gauss6')
+    s = np.array(domain.project(geom, geometry = geom, onto = basis, ischeme = 'gauss6', constraints = cons))
+    return ut.dummy_go.with_mapping(s,cons, knots = kv)
+
+
+def circle_go(ndims,p):
+    kv = np.prod([ut.nonuniform_kv(p, knotvalues = np.linspace(0,1,n+1)) for n in ndims])
+    domain, geom = mesh.rectilinear(kv.knots)
+    basis = domain.basis('bspline', degree = p, knotvalues = kv.knots).vector(2)
+    x, y = 2*geom[0] - 1, 2*geom[1] - 1
+    func = function.stack([1/2*x*function.sqrt(1 - y**2/2)+1/2*1, 1/2*y*function.sqrt(1 - x**2/2)+1/2*1])
+    cons = domain.boundary.project(func, geometry = geom, onto = basis, ischeme = 'gauss6')
+    s = np.array(domain.project(func, geometry = geom, onto = basis, ischeme = 'gauss6', constraints = cons))
+    return ut.dummy_go.with_mapping(s,cons, knots = kv)
+
+
+
+def cube_go(ndims,p):
+    kv = ut.tensor_kv(*[ut.nonuniform_kv(p, knotvalues = np.linspace(0,1,n+1)) for n in ndims])
+    domain, geom = mesh.rectilinear(kv.knots)
+    basis = domain.basis('bspline', degree = p, knotvalues = kv.knots).vector(3)
+    cons = domain.boundary.project(geom, geometry = geom, onto = basis, ischeme = 'gauss6')
+    s = np.array(domain.project(geom, geometry = geom, onto = basis, ischeme = 'gauss6', constraints = cons))
+    return ut.tensor_grid_object.with_mapping(s,cons, knots = kv)
+
+
+def O_grid(ndims,p, inner = 1, outer = 2):
+    kv = ut.tensor_kv([ut.nonuniform_kv(p, knotvalues = np.linspace(0,1,ndims[i]+1), periodic = (i == 1)) for i in range(2)])
+    go = ut.tensor_grid_object(knots = kv)
+    def circle(R):
+        return lambda g: R*function.stack([function.cos(2*np.pi*g[1]), function.sin(2*np.pi*g[1])])
+    R1, R2 = inner, outer
+    func = (1 - go.geom[0])*circle(R1)(go.geom) + go.geom[0]*circle(R2)(go.geom)
+    cons = util.NanVec(2*len(go.basis))
+    for side in ['left', 'right']:
+        cons |= go.domain.boundary[side].project(func, geometry = go.geom, onto = go.basis.vector(2), ischeme = 'gauss6')
+    go.cons = cons
+    go.s = go.project(func, onto = go.basis.vector(2))
+    return go
+
+def dummy_go(ndims, p):
+    go = circle_go(ndims,p)
+    dgo = ut.dummy_go(**go.instantiation_lib)
+    dgo.s = go.s
+    dgo.cons = go.cons
+    return dgo
